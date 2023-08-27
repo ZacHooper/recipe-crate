aa71b49e68e4a01ab36032402e7001772225e0c4
Zac Hooper <zac.g.hooper@gmail.com>
2023-08-27 10:32:25 +1000

    Remove Text to Image stuff

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
+import base64
+import io
 import json
 import logging
 import os
 import platform
-import base64
-import io
 
-import cv2
 import numpy as np
 from langchain.chains import LLMChain
+from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
-from langchain.output_parsers import PydanticOutputParser, OutputFixingParser
+from langchain.output_parsers import OutputFixingParser, PydanticOutputParser
 from langchain.prompts import PromptTemplate
 from langchain.prompts.chat import (
     ChatPromptTemplate,
@@ -17,8 +17,7 @@ from langchain.prompts.chat import (
     SystemMessagePromptTemplate,
 )
 from pydantic import BaseModel, Field, validator
-from pytesseract import pytesseract
-from supabase import create_client, Client
+from supabase import Client, create_client
 
 from utils.aws import get_secret_dict
 
@@ -66,39 +65,8 @@ class Recipe(BaseModel):
 
 
 def lambda_handler(event, context):
-    # Get Image from event
-    encoded_image = event["body"]["image"]
-
-    # Decode image
-    image_data = base64.b64decode(encoded_image)
-    nparr = np.frombuffer(image_data, np.uint8)
-
-    # Read Image
-    path_to_tesseract = "/opt/homebrew/opt/tesseract/bin/tesseract"
-    pytesseract.tesseract_cmd = path_to_tesseract
-
-    # decode image
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
-    # --- dilation on the green channel ---
-    logger.info("Processing image")
-    dilated_img = cv2.dilate(img[:, :, 1], np.ones((7, 7), np.uint8))
-    bg_img = cv2.medianBlur(dilated_img, 21)
-
-    # --- finding absolute difference to preserve edges ---
-    diff_img = 255 - cv2.absdiff(img[:, :, 1], bg_img)
-
-    # --- normalizing between 0 to 255 ---
-    norm_img = cv2.normalize(
-        diff_img, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1
-    )
-
-    # --- Otsu threshold ---
-    th = cv2.threshold(norm_img, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-
-    # Extract Text
-    logger.info("Extracting text")
-    text = pytesseract.image_to_string(th)
+    # Get text from event
+    text = event["body"]["text"]
 
     # Define Response Schema
     logger.info("Building Prompt")
@@ -117,14 +85,6 @@ def lambda_handler(event, context):
             3. The method
             4. The blurb -> a short description of the recipe
             5. Any other information -> for example how many it serves, or what page it's on, etc.
-
-            Sometimes you may come across some text that hasn't parsed correctly.
-            For example:
-            - \u2019 -> '
-            - \u201c -> "
-            - \u201d -> "
-
-            If you come across any of these, you can replace them with the correct character.
 
             {format_instructions}
             
@@ -174,15 +134,32 @@ def lambda_handler(event, context):
 
 
 if __name__ == "__main__":
-    image_path = "sample_data/IMG_0167 copy.png"
-    logger.info(f"Reading image: {image_path}")    
-
-    # Open image and convert to base64
-    with open(image_path, "rb") as f:
-        image = base64.b64encode(f.read())
-    
-    # Create event
-    event = {"body": {"image": image}}
+    text = """Cinnamon & cherry tomato koshari
+SERVES 6
+½ cup green/brown lentils (150g)
+750g fresh cherry tomatoes or 2 × 400g tins cherry tomatoes, drained
+4 cloves of garlic
+6 shallots, very thinly sliced
+5 tablespoons extra virgin olive oil
+a small bunch of coriander,
+including stalks
+1 teaspoon ground allspice or
+4 allspice berries, bashed
+1 stick of cinnamon
+1 cup of rice (300g), rinsed rapeseed oil
+a small bunch of parsley, roughly chopped a small bunch of mint, roughly chopped
+This bravely spiced rice and lentil dish is more than a sum of its parts and comes (mostly) from the store cupboard. It's topped with a crown of crispy frizzled shallots, which brings a key level of crunch and contrast to what is otherwise soft and comforting. In its pure Egyptian street-food form there is pasta, macaroni to be precise, and often chickpeas, too.
+I've just used rice and lentils here for simplicity, but you could add a handful of macaroni to the rice, or a drained can of chickpeas, to make
+it a little more authentic.
+First, soak your lentils in 300ml of warm water for an hour, then drain and rinse. Preheat the oven to 200°C/180°C fan/gas 6.
+In a large cast-iron pot with a lid, put the tomatoes, garlic, a quarter of the sliced shallots and 2 tablespoons of the olive oil. Chop the coriander stalks then add those as well, along with the allspice and stick of cinnamon.
+Roast in the oven for 10 minutes then remove from the oven and sprinkle on the rice and rinsed lentils. Add 900ml of boiling water, season with salt, cover with the lid and place in the oven for another 25 minutes.
+Meanwhile, make the crispy shallots. Fill a saucepan with a 3cm depth of room-temperature rapeseed oil. Put over a high heat and then, once hot, add the remaining shallots. You should see the oil bubble up when you add them. Give them a quick stir so they are all evenly submerged in the oil, then leave to cook for around 4 minutes, keeping an eye on them as they cook. Once they start turning a light golden colour remove them from the oil with a slotted spoon and onto some kitchen paper then leave to cool completely.
+Remove the rice from the oven and, using an oven glove, take off the lid and remove the whole garlic cloves. Let the rice stand for 10 minutes and, meanwhile, pop the garlic out of its skin and mash with some sea salt and the remaining 3 tablespoons extra virgin olive oil.
+Stir the garlic oil through the rice followed by the chopped parsley, mint and coriander leaves. Season with salt and pepper and top with the crispy shallots.
+42
+ONE • POT"""
+    event = {"body": {"text": text}}
 
     # Run Lambda
     res = lambda_handler(event, None)
@@ -190,28 +167,6 @@ if __name__ == "__main__":
     recipe = json.loads(res["body"])
 
     logger.info(f"Writing output to database...")
-    data, count = supabase.table('recipes').insert(recipe).execute()
+    data, count = supabase.table("recipes").insert(recipe).execute()
 
     print(data)
-
-
-    # with open(f"output/{image_path.replace('sample_data/', '')}_2.json", "r") as f:
-    #     logger.info(f"Writing output to: {f.name}")
-    #     f.write(res["body"])
-
-
-    # # Get all files im sample_data/recipes
-    # for filename in os.listdir("sample_data/recipes"):
-    #     image_path = f"sample_data/recipes/{filename}"
-    #     logger.info(f"Reading image: {image_path}")
-    #     # Open image and convert to base64
-    #     with open(image_path, "rb") as f:
-    #         image = base64.b64encode(f.read())
-    #     # Create event
-    #     event = {"body": {"image": image}}
-    #     # Run Lambda
-    #     res = lambda_handler(event, None)
-
-    #     with open(f"output/{filename}.json", "w") as f:
-    #         logger.info(f"Writing output to: {f.name}")
-    #         f.write(res["body"])
